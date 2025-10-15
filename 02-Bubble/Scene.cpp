c2393e977ee2fd9e0678b1ed11066ecaea49cf92
Enric Segarra <ensecal@gmail.com>
2025-10-15 23:24:52 +0200

    level04

--- 02-Bubble/Scene.cpp
+++ 02-Bubble/Scene.cpp
@@ -35,7 +35,7 @@ void Scene::init()
 {
 	initShaders();
 	CurrentMap = 1;
-	map = TileMap::createTileMap("levels/level04.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
+	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
 	player = new Player();
 	enemy = new Enemy();
 	enemy->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram, 0);

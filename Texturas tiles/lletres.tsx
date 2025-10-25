2bab6dc0e7686df4abc46ac096af8d1153ec40de
Enric Segarra <ensecal@gmail.com>
2025-10-25 12:09:04 +0200

    .

---
+++ Texturas tiles/lletres.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="lletres" tilewidth="8" tileheight="8" margin="2" tilecount="555" columns="37">
+ <image source="../02-Bubble/images/lletres.png" trans="ff00ff" width="301" height="127"/>
+</tileset>
